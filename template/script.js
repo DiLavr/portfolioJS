e109cbfd23359140b7c849c97ba8137b7a31c9a5
Dmitrii Lavruk <dmitry@lavruk.com>
2022-10-20 19:20:10 -0400

    connecting API Quotes - not working

---
+++ template/script.js
@@ -0,0 +1,22 @@
+let apiQuotes = [];
+// show new quote
+function newQuote(){
+    // rendomizer in Quote Api arraw
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+const response = await fetch(apiUrl);
+apiQuotes = await response.json();
+newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// on load
+getQuotes();
