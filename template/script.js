7e1cb32c8a346af1fd3f9fca018a53a3235aea8e
Dmitrii Lavruk <dmitry@lavruk.com>
2022-10-20 19:23:05 -0400

    alternative API for quotes

--- template/script.js
+++ template/script.js
@@ -9,6 +9,7 @@ function newQuote(){
 // Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    // Alternative API https://zenquotes.io/api/quotes
     try {
 const response = await fetch(apiUrl);
 apiQuotes = await response.json();
