d7d0a5fff7b1c2756d5177115cb3aa34e7418ce4
Dmitrii Lavruk <dmitry@lavruk.com>
2022-10-20 20:36:02 -0400

    adding loader for slow internet

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -4,12 +4,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-// const loader = document.getElementById('loader');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+    loading();
     // rendomizer in Quote Api arraw
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -25,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     // Alternative API https://zenquotes.io/api/quotes
     try {
@@ -41,7 +57,6 @@ newQuote();
     }
 }
 
-
 // Tweet a quote
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
@@ -51,5 +66,7 @@ function tweetQuote() {
 // event listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
+
 // on load
 getQuotes();
+// loading();
