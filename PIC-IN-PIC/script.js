a8a44a3b8ff9acd0f806a9d91cc410bd4854ce7c
DiLavr <42397553+DiLavr@users.noreply.github.com>
2022-11-05 22:43:19 -0400

    init js function for pic-in-pic

---
+++ PIC-IN-PIC/script.js
@@ -0,0 +1,16 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Promt user to select a video strime, pass video to element, then play video
+
+async function selectMediaStream() {
+  try {
+    something;
+  } catch (error) {
+    // Catch error here
+    console.log("whoops, error here:", error);
+  }
+}
+
+// on load
+selectMediaStream();
