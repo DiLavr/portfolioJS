a4d4a4473dbd93833ff9ef676a55a574ec9c5290
DiLavr <42397553+DiLavr@users.noreply.github.com>
2022-11-05 23:11:07 -0400

    picinpic is not working

--- PIC-IN-PIC/script.js
+++ PIC-IN-PIC/script.js
@@ -1,16 +1,30 @@
-const videoElement = document.getElementById("video");
-const button = document.getElementById("button");
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
 // Promt user to select a video strime, pass video to element, then play video
 
 async function selectMediaStream() {
   try {
-    something;
+    // screen capture API
+    const mediaSteam = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaSteam;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
   } catch (error) {
     // Catch error here
-    console.log("whoops, error here:", error);
+    console.log('whoops, error here:', error);
   }
 }
+
+button.addEventListener('click', async () => {
+    // disable the button
+    button,disabled = true;
+    // start pic in pic
+    await videoElement.requestPictureInPicture();
+    // reset Button
+    button.desabled = false;
+});
 
 // on load
 selectMediaStream();
