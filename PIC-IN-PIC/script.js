d9e7443ad4cfc981f662fe7781e067d913395f81
Dmitrii Lavruk <dmitry@lavruk.com>
2023-05-30 22:43:37 -0400

    new style2.css for new project UI

--- PIC-IN-PIC/script.js
+++ PIC-IN-PIC/script.js
@@ -1,8 +1,17 @@
 
+/**
+ * Retrieves the video element and button element from the DOM.
+ * @returns {Object} An object containing the video element and button element.
+ */
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
+/**
+ * Asynchronously selects a media stream from the user's display and sets it as the source
+ * for the video element.
+ * @returns None
+ */
 async function selectMediaStream() {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
@@ -15,7 +24,13 @@ async function selectMediaStream() {
   }
 }
 
-button.addEventListener('click', async () => {
+/**
+ * Adds an event listener to the button that listens for a click event and then
+ * executes an asynchronous function.
+ * @param {function} asyncFunction - the asynchronous function to execute when the button is clicked
+ * @returns None
+ */
+button.addEventListener('click', async () => { 
   // Disable Button
   button.disabled = true;
   // Start Picture in Picture
